448ee534a52dc20ce74a1e27d56ce50eab35af61
Lucas <lucaspellegrinetti@icloud.com>
2025-06-02 22:15:56 -0300

    subindo addon3

---
+++ addon/skips_maps/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+
+client_script 'client/*.lua'
+
+files {
+    'interiorproxies.meta',
+    'stream/vesp_props.ytyp',
+    'stream/lafa2k_favelafix.ytyp',
+    'stream/props.ytyp',
+    'stream/v_int_40.ytyp',
+} 
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/vesp_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/lafa2k_favelafix.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_int_40.ytyp'
---
+++ addon/vrp_motos/fxmanifest.lua
@@ -0,0 +1,157 @@
+fx_version 'adamant'
+game 'gta5'
+ 
+files {
+    'data/biz25/vehicles.meta',
+    'data/biz25/carvariations.meta',
+    'data/bmw_1000/vehicles.meta',
+    'data/bmw_1000/carvariations.meta',
+    'data/bmw_1000/carcols.meta',
+    'data/bmw_1000/handling.meta',
+    'data/cbr1000/vehicles.meta',
+    'data/cbr1000/carvariations.meta',
+    'data/cbr1000/carcols.meta',
+    'data/cbr1000/handling.meta',
+    'data/cbr2017/vehicles.meta',
+    'data/cbr2017/carvariations.meta',
+    'data/cbr2017/carcols.meta',
+    'data/cbr2017/handling.meta',
+    'data/cbtwister/vehicles.meta',
+    'data/cbtwister/carvariations.meta',
+    'data/cbtwister/carcols.meta',
+    'data/cbtwister/handling.meta',
+    'data/cg150/vehicles.meta',
+    'data/cg150/carvariations.meta',
+    'data/cg150/carcols.meta',
+    'data/cg150/handling.meta',
+    'data/cgifood/vehicles.meta',
+    'data/cgifood/carvariations.meta',
+    'data/cgifood/carcols.meta',
+    'data/cgifood/handling.meta',
+    'data/ducati/vehicles.meta',
+    'data/ducati/carvariations.meta',
+    'data/ducati/carcols.meta',
+    'data/ducati/handling.meta',
+    'data/harley/vehicles.meta',
+    'data/harley/carvariations.meta',
+    'data/harley/carcols.meta',
+    'data/harley/handling.meta',
+    'data/hayabusa/vehicles.meta',
+    'data/hayabusa/carvariations.meta',
+    'data/hayabusa/carcols.meta',
+    'data/hayabusa/handling.meta',
+    'data/ninja/vehicles.meta',
+    'data/ninja/carvariations.meta',
+    'data/ninja/carcols.meta',
+    'data/ninja/handling.meta',
+    'data/ninjazx10/vehicles.meta',
+    'data/ninjazx10/carvariations.meta',
+    'data/ninjazx10/carcols.meta',
+    'data/ninjazx10/handling.meta',
+    'data/r1/vehicles.meta',
+    'data/r1/carvariations.meta',
+    'data/r1/carcols.meta',
+    'data/r1/handling.meta',
+    'data/r6/vehicles.meta',
+    'data/r6/carvariations.meta',
+    'data/r6/carcols.meta',
+    'data/r6/handling.meta',
+    'data/rc390/vehicles.meta',
+    'data/rc390/carvariations.meta',
+    'data/rc390/carcols.meta',
+    'data/rc390/handling.meta',
+    'data/surfboard/vehicles.meta',
+    'data/surfboard/carvariations.meta',
+    'data/surfboard/carcols.meta',
+    'data/surfboard/handling.meta',
+    'data/titan160/vehicles.meta',
+    'data/titan160/carvariations.meta',
+    'data/titan160/carcols.meta',
+    'data/titan160/handling.meta',
+    'data/tmax500/vehicles.meta',
+    'data/tmax500/carvariations.meta',
+    'data/tmax500/carcols.meta',
+    'data/tmax500/handling.meta',
+    'data/z1000/vehicles.meta',
+    'data/z1000/carvariations.meta',
+    'data/z1000/carcols.meta',
+    'data/z1000/handling.meta',
+}
+ 
+data_file 'VEHICLE_METADATA_FILE' 'data/biz25/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/biz25/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/bmw_1000/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/bmw_1000/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/bmw_1000/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/bmw_1000/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/cbr1000/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/cbr1000/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/cbr1000/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/cbr1000/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/cbr2017/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/cbr2017/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/cbr2017/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/cbr2017/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/cbtwister/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/cbtwister/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/cbtwister/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/cbtwister/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/cg150/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/cg150/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/cg150/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/cg150/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/cgifood/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/cgifood/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/cgifood/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/cgifood/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/ducati/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/ducati/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/ducati/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/ducati/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/harley/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/harley/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/harley/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/harley/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/hayabusa/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/hayabusa/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/hayabusa/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/hayabusa/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/ninja/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/ninja/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/ninja/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/ninja/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/ninjazx10/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/ninjazx10/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/ninjazx10/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/ninjazx10/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/r1/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/r1/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/r1/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/r1/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/r6/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/r6/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/r6/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/r6/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/rc390/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/rc390/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/rc390/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/rc390/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/surfboard/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/surfboard/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/surfboard/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/surfboard/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/titan160/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/titan160/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/titan160/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/titan160/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/tmax500/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/tmax500/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/tmax500/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/tmax500/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/z1000/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/z1000/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/z1000/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/z1000/carvariations.meta'
+
+
+
